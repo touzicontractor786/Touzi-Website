6bbaafada22b9929869340a29a57a9f66884e1bc
touzicontractor786 <132910909+touzicontractor786@users.noreply.github.com>
2023-05-11 14:53:45 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,5 +1,15 @@
+
 //initialize firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyA3smKQgj9SAR9Q9b4Nh4pUU26eoQ7Kk6E",
+  authDomain: "portfolio-106e5.firebaseapp.com",
+  databaseURL: "https://portfolio-106e5-default-rtdb.firebaseio.com",
+  projectId: "portfolio-106e5",
+  storageBucket: "portfolio-106e5.appspot.com",
+  messagingSenderId: "977842486384",
+  appId: "1:977842486384:web:538123598c376df492c8b5",
+  measurementId: "G-75J6YWC13E"
+};
 
 // reference your database
 var contactFormDB = firebase.database().ref("contactForm");
